3606d129b557b56cdc490c3de9acf323421a0a50
makamaka <makamaka.donzoko@gmail.com>
2010-08-19 17:15:33 +0900

    updated Changes and version

--- lib/JSON.pm
+++ lib/JSON.pm
@@ -7,7 +7,7 @@ use base qw(Exporter);
 @JSON::EXPORT = qw(from_json to_json jsonToObj objToJson encode_json decode_json);
 
 BEGIN {
-    $JSON::VERSION = '2.21';
+    $JSON::VERSION = '2.21_001';
     $JSON::DEBUG   = 0 unless (defined $JSON::DEBUG);
 }
 

3763298c77eebd035148311c04c759401440e92f
Kenichi Ishigaki <ishigaki@cpan.org>
2022-10-09 08:55:58 +0900

    Update backportPP with JSON::PP 4.12

--- lib/JSON/backportPP.pm
+++ lib/JSON/backportPP.pm
@@ -15,7 +15,7 @@ use JSON::backportPP::Boolean;
 use Carp ();
 #use Devel::Peek;
 
-$JSON::backportPP::VERSION = '4.11';
+$JSON::backportPP::VERSION = '4.12';
 
 @JSON::PP::EXPORT = qw(encode_json decode_json from_json to_json);
 
